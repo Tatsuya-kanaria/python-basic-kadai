95ae9ba293b8977f1722617ff3313d932aeea080
Tatsuya-kanaria <kanaria.git@gmail.com>
2023-06-07 18:21:40 +0900

    modify 環境依存文字の削除

--- kadai_005/area_of_trapezoid.py
+++ kadai_005/area_of_trapezoid.py
@@ -2,18 +2,18 @@
 
 def area_of_trapezoid(top, bottom, height):
     '''
-    台形の面積（㎠）を返す
+    台形の面積（cm2）を返す
     top: 上辺(cm)
     bottom: 下辺(cm)
     height: 高さ(cm)
 
     公式：台形の面積 =（上辺＋下辺）× 高さ ÷ 2
 
-    return: '{} ㎠'.format(area)
+    return: '{} cm2'.format(area)
     '''
 
     area = (top + bottom) * height / 2
-    return '{} ㎠'.format(area)
+    return '{} cm2'.format(area)
 
 area =  area_of_trapezoid(top=10, bottom=20, height=5)
 
